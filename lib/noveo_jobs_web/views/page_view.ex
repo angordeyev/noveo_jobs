f88812c5c31d1fe899b0b6f84fdf17330922ab4f
angordeyev <angordeyev@gmail.com>
2020-02-07 20:54:04 +0300

    table page

--- lib/noveo_jobs_web/views/page_view.ex
+++ lib/noveo_jobs_web/views/page_view.ex
@@ -1,3 +1,12 @@
 defmodule NoveoJobsWeb.PageView do
   use NoveoJobsWeb, :view
+
+  def header do
+    ProfessionsReport.init_table() |> ProfessionsReport.render() |> hd
+  end
+
+  def table do
+    ProfessionsReport.init_table() |> ProfessionsReport.render() |> Enum.drop(1)
+  end
+
 end
